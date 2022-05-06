424b99caea9d579047cd5d23e64ef03ceab69881
Andre Veziane <andre.veziane@mercadolivre.com>
2022-05-05 23:23:21 -0300

    fix: added javadoc

--- src/main/java/com/piml/gandalf/gandalf/controller/UserController.java
+++ src/main/java/com/piml/gandalf/gandalf/controller/UserController.java
@@ -24,6 +24,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /**POST Route to create a user
+     * @param dto receives information on body necessary to create an user
+     *            name, cpf, username, password, email, role and in case of
+     *            the user be a warehouse agent need to pass the warehouse id.
+     * @return return the created user without the password for security reasons.
+     */
     @ApiOperation(value = "Register a new User")
     @PostMapping("/user/v1")
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO dto) {
@@ -31,6 +37,11 @@ public class UserController {
         UserDTO createdUser = UserDTO.map(userService.create(user));
         return new ResponseEntity<UserDTO>(createdUser, HttpStatus.CREATED);
     }
+
+    /**GET Route to get a user by id
+     * @param id receives an user id
+     * @return returns the user if he exists
+     */
     @ApiOperation(value = "Find User by ID")
     @GetMapping("/user/v1/{id}")
     public ResponseEntity<UserDTO> getById(@PathVariable Long id){
@@ -38,6 +49,13 @@ public class UserController {
         UserDTO convertedProduct = UserDTO.map(user);
         return ResponseEntity.ok(convertedProduct);
     }
+
+    /**POST Route Login method
+     * @param dto receives username and password
+     * @return return a phrase simulating the token (not implemented) in case of success.
+     * @throws UnsupportedEncodingException encryption exception .
+     * @throws NoSuchAlgorithmException encryption exception .
+     */
     @ApiOperation(value = "Performs a Sign In")
     @PostMapping("/user/v1/signin")
     public SignInResponseDTO signIn(@Valid @RequestBody SignInDTO dto) throws UnsupportedEncodingException, NoSuchAlgorithmException {
